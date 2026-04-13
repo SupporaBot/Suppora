dceaab3b4380cbdf4f145bf6d18b7346fee0b55d
br4dyb <halting.credo.00@icloud.com>
2026-04-13 10:23:47 -0500

    fix(bot): cors allowed origins

--- apps/bot/src/api/middleware/cors.ts
+++ apps/bot/src/api/middleware/cors.ts
@@ -2,17 +2,28 @@ import cors, { CorsOptions } from "cors"
 
 const ENVIRONMENT = process.env.ENVIRONMENT
 
-
+// Allowed Origins:
 const allowedOrigins = [
-    'https://brilliant-austina-sessions-bot-discord-5fa4fab2.koyeb.app',
-    'https://sessionsbot.fyi',
-    'https://www.sessionsbot.fyi',
+    'https://suppora.app',
+    'https://suppora.pages.dev',
+    ...ENVIRONMENT != 'production'
+        ? [
+            'http://localhost:3000',
+            'http://localhost:5173'
+        ]
+        : []
 ]
-if (ENVIRONMENT != 'production') {
-    allowedOrigins.push(
-        'http://localhost:3000',
-        'http://localhost:5173'
-    )
+
+// Util - Check for ALLOWED Origin/Host:
+function isAllowedOrigin(org: string) {
+    const originUrl = new URL(org)
+    const host = originUrl.hostname
+
+    // Exact Allowed Matches:
+    if (allowedOrigins?.includes(originUrl.href)) return true
+
+    // Allow CloudFlare Preview Pages:
+    if (host.endsWith('.suppora.pages.dev')) return true
 }
 
 const options: CorsOptions = {
@@ -21,12 +32,12 @@ const options: CorsOptions = {
             // Allow requests like Postman or curl (no origin)
             return callback(null, true);
         }
-        if (allowedOrigins.includes(requestOrigin)) {
-            // Allow whitelisted origins
+        if (isAllowedOrigin(requestOrigin)) {
+            // Allow specified origins
             return callback(null, true);
         }
         // Block everything else
-        return callback(new Error(`[CORS] Request origin NOT allowed! - Origin: ${requestOrigin}`));
+        return callback(new Error(`[CORS] Not allowed: ${requestOrigin}`))
     },
     credentials: true
 }
